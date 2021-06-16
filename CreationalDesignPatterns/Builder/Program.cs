4d776e2e33db9d23a02454fef3bde07b303661b3
Arkadiusz Urbankiewicz <Arkadiusz.Urbankiewicz@duni.com>
2021-06-16 12:17:50 +0200

    add SingleResponsibilityPrinciple

--- CreationalDesignPatterns/Builder/Program.cs
+++ CreationalDesignPatterns/Builder/Program.cs
@@ -6,7 +6,7 @@ namespace Builder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Builder");
         }
     }
 }
