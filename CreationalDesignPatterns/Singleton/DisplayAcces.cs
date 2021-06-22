0488ec68673f9fb3eb72346cbbb4d71dc299d799
Arkadiusz Urbankiewicz <Arkadiusz.Urbankiewicz@duni.com>
2021-06-22 13:07:42 +0200

    add singleton

---
+++ CreationalDesignPatterns/Singleton/DisplayAcces.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class DisplayAcces
+    {
+        private static DisplayAcces display;
+        public int  DisplayId { get; set; }
+
+        private  DisplayAcces()
+        {
+            //no possibility to create instance outside
+        }
+        public void Display(string msg)
+        {
+            Console.WriteLine($"Display will print: {msg}");
+        }
+        public static  DisplayAcces GetInstance()
+        {
+            if(display is null)
+            {
+                display = new DisplayAcces();
+            }
+
+            return display;
+        }
+
+
+    }
+}
---
+++ CreationalDesignPatterns/Singleton/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            DisplayAcces displayForUser1 = DisplayAcces.GetInstance();
+            DisplayAcces displayForUser2 = DisplayAcces.GetInstance();
+
+            displayForUser1.DisplayId = 12;
+            displayForUser2.Display(Convert.ToString(displayForUser2.DisplayId));
+
+            if(ReferenceEquals(displayForUser1, displayForUser2))
+            {
+                Console.WriteLine("Yes! Singleton");
+            }
+            else
+            {
+                Console.WriteLine("no this obj are different");
+            }
+        }
+    }
+}
